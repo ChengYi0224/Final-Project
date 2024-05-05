e69c5d826a2720131081808e0ed8e107f7722d7e
Ting-An Hung <annhung323451@gmail.com>
2024-05-05 17:46:51 +0800

    Update main.c

--- main.c
+++ main.c
@@ -44,13 +44,14 @@ int main(int argc, char const *argv[])
     SDL_Rect sceneRect = {0, 0, winW, winH};
     // dialogue顯示方框
     SDL_Rect dialRect = {30, 10 + winH * 2 / 3, winW - 60, winH / 3 - 30};
-    // 文字 ?需判斷換行、字數?
+    // 文字
     SDL_Rect textRect = {dialRect.x + 15, dialRect.y + 3, dialRect.w - 30, dialRect.h - 6};
     // 當前scene的路徑
 
     SDL_Event event;
     int32_t game_is_running = 1;
     int32_t ptsize = 40; //測試用
+    char * text = "abcdefu rah rah ah ah ah roma roma-ma gaga ooh -la-la ghijk lmnopq";
     TTF_Font * font = TTF_OpenFont( "assets/fonts/Amiri-Bold.ttf" , ptsize); //測試用
     SDL_Color color = {255, 255, 255}; //測試用
     // 遊戲主迴圈
@@ -80,7 +81,7 @@ int main(int argc, char const *argv[])
         //textRect.w = winW / 100 * strlen("abcdefu");
         //textRect.h = winH / 15;
         DisplayImg(renderer, imgtest2, NULL, &dialRect);
-        DisplayText(renderer, "abcdefu rah rah ah ah ah", font, color, &textRect); // 對話
+        DisplayText(renderer, text, font, color, &textRect); // 對話
 
         // 繪製選項
         // for(size_t i = 0; i < (optionNum); i++){
