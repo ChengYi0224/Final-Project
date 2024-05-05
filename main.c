9d767d45ea8fc0b4847870e926c85750904856ed
Ting-An Hung <annhung323451@gmail.com>
2024-05-05 16:57:39 +0800

    Update main.c

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <SDL2/SDL_mixer.h>
 
 #define imgtest "assets/scenes/2K.jpg"
+#define imgtest2 "assets/scenes/aut.jpg"
 #define ScriptPath "ta.toml"
 
 int main(int argc, char const *argv[])
@@ -35,16 +36,23 @@ int main(int argc, char const *argv[])
     int32_t winW = 1280, winH = 720; // width and height of window
     SDL_Window *GameWindow = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, winW, winH, 0);
     // 建立渲染器
-    SDL_Renderer *renderer = SDL_CreateRenderer(GameWindow, -1, );
+    SDL_Renderer *renderer = SDL_CreateRenderer(GameWindow, -1, 0);
     printf("Debug msg 4\n");
 
     // 圖形介面排版初始化 (建立各個物件需要用的方框)
     // scene顯示方框
     SDL_Rect sceneRect = {0, 0, winW, winH};
     // dialogue顯示方框
-    SDL_Rect dialRect = {10, 10 + winH / 3, winW - 80, winH / 3};
+    SDL_Rect dialRect = {30, 10 + winH * 2 / 3, winW - 60, winH / 3 - 30};
+    // 文字 ?需判斷換行、字數?
+    SDL_Rect textRect = {dialRect.x + 15, dialRect.y + 3, dialRect.w - 30, dialRect.h - 6};
     // 當前scene的路徑
 
+    SDL_Event event;
+    int32_t game_is_running = 1;
+    int32_t ptsize = 40; //測試用
+    TTF_Font * font = TTF_OpenFont( "assets/fonts/Amiri-Bold.ttf" , ptsize); //測試用
+    SDL_Color color = {255, 255, 255}; //測試用
     // 遊戲主迴圈
     while (1)
     {
@@ -61,21 +69,37 @@ int main(int argc, char const *argv[])
         // # 畫面繪製
 
         // 繪製圖像
-        DisplayImg(renderer, imgtest, NULL, &sceneRect); // 背景
+        // 背景
+        DisplayImg(renderer, imgtest, NULL, &sceneRect);//
         // DisplayImg(); // 立繪
         // DisplayImg(); // 物品欄
         // DisplayImg(); // 角色頭像
         // DisplayImg(); // 角色頭像邊框
 
         // 繪製文字
-        // DisplayText(); // 對話
+        //textRect.w = winW / 100 * strlen("abcdefu");
+        //textRect.h = winH / 15;
+        DisplayImg(renderer, imgtest2, NULL, &dialRect);
+        DisplayText(renderer, "abcdefu rah rah ah ah ah", font, color, &textRect); // 對話
 
         // 繪製選項
         // for(size_t i = 0; i < (optionNum); i++){
         //     DisplayButton(); // option
         // }
 
-        // # 聆聽事件(偵測滑鼠/鍵盤輸入) 包含音效
+        // # 聆聽事件(偵測滑鼠/鍵盤輸入) 包含音效  //目前只支援關閉視窗
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    game_is_running = 0;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+                        game_is_running = 0;
+                    }
+                    break;
+            }
+        }
         // 點擊選項
         // 物品預覽
 
@@ -83,7 +107,7 @@ int main(int argc, char const *argv[])
         SDL_RenderPresent(renderer);
 
         // # 終止條件
-        if(1) break;
+        if(!game_is_running) break;
     }
 
     // 程式結束，以相反順序釋放資源
